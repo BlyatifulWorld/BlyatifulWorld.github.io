5f56b3ba5b3d25cafa26ed83768deedfe2bb1d29
Korakoch <korakoch.mo@ku.th>
2023-10-09 09:19:12 +0700

    fist commit

---
+++ src/components/main.js
@@ -0,0 +1,175 @@
+const tableSC1 = [["ภาควิชาวิทยาศาสตร์", "ห้องชัยพฤกษ์"], ["สาขาวิชาพันธุศาสตร์"], ["สาขาวิชาสัตววิทยา"]];
+const tableSC2 = [["สาขาวิชาพันธุศาสตร์"],["สาขาวิชาชีววิทยา"],["สาขาวิชาชีววิทยา","หลักสูตร วท.บ(วิทยาศาสตร์ชีวภาพ)"]]
+const tableSC3 = [["สาขาวิชาชีวเคมี"],["โครงการจัดตั้งภาควิชาพฤกษศาสตร์"],["โครงการจัดตั้งภาควิชาพฤกษศาสตร์"]]
+const tableSC4 = [["สำนักงานภาควิชาวิทยาศาสตร์","ห้องปฏิบัติการคอมพิวเตอร์ (ส่วนกลาง)"],["สาขาวิชาชีววิทยา"],["หลักสูตร วท.บ.(วิทยาศาสตร์ชีวภาพ)","หลักสูตร วท.ม.(วิทยาศาสตร์ชีวผลิตภัณฑ์)"]]
+const tableSC5 = [["ห้องปฏิบัติการสาขาวิชาเคมี"],["ห้องปฏิบัติการสาขาวิชาเคมี"],["ห้องปฏิบัติการสาขาวิชาเคมี"]]
+const tableSC9 = [["ห้องคณบดีคณะศิลปศาสตร์และวิทยาศาสตร์","สำนักงานเลขานุการคณะศิลปศาสตร์และวิทยาศาสตร์","ห้องงานบริการการศึกษา","ห้องงานคลังและพัสดุ","ห้องสมุด","ห้องประชุม"],["ภาควิชาศิลปศาสตร์"],["ภาควิชาคณิตศาสตร์ สถิติ และคอมพิวเตอร์","โครงการจัดตั้งภาควิชาคอมพิวเตอร์"],["ห้องเรียนภาควิชาศิลปศาสตร์","ห้องเรียนโครงการจัดตั้งภาควิชาเทคโนโลยีสารสนเทศและการสื่อสาร"],["โครงการจัดตั้งภาควิชาสังคมศาสตร์","โครงการจัดตั้งภาควิชาเทคโนโลยีการบัญชี","โครงการจัดตั้งภาควิชาการจัดการธุรกิจ"]]
+const tableSC11 = [["สำนักงานโครงการจัดตั้งภาควิชาเทคโนโลยีสารสนเทศและการสื่อสาร","สำนักงานโครงการปริญญาตรี สาขาวิชาเทคโนโลยีสารสนเทศ (ภาคพิเศษ)","ห้องเรียนโครงการจัดตั้งภาควิชาเทคโนโลยีสารสนเทศและการสื่อสาร"],["ห้องปฏิบัติการคอมพิวเตอร์","ห้องเรียนรู้ด้วยตนเอง"],["ห้องปฏิบัติการคอมพิวเตอร์"],["ห้องบรรยาย"]]
+const tableSC14 = [["ศูนย์ส่งเสริมการวิจัยและถ่ายทอดเทคโนโลยี", "ห้องโครงการหลักสูตรวิทยาศาสตรมหาบัณฑิต สาขาวิชานิติวิทยาศาสตร์ (ภาคพิเศษ)", "ห้องปฏิบัติการคอมพิวเตอร์ (ส่วนกลางคณะฯ)","สำนักงานโครงการจัดตั้งภาควิชาการตลาดและโลจิสติกส์","ห้องประชุม"], ["สำนักงานโครงการจัดตั้งภาควิชาเคมี","สำนักงานโครงการจัดตั้งภาควิชาฟิสิกส์","ห้องปฏิบัติการสาขาวิชาเคมี","ห้องปฏิบัติการสาขาวิชาฟิสิกส์"],["ห้องปฏิบัติการสาขาวิชาเคมี","ห้องปฏิบัติการสาขาวิชาฟิสิกส์"],["สำนักงานโครงการจัดตั้งภาควิชาจุลชีววิทยา"],["ห้องปฏิบัติการสาขาวิชาจุลชีววิทยา","ห้องเรียนบรรยาย (ส่วนกลางคณะฯ)","ห้องออดิทอเรียม"],["ห้องเรียนบรรยาย (ส่วนกลางคณะฯ)"]]
+const tableLLB = [["สำนักงานโครงการจัดตั้งภาควิชาภาษาอังกฤษ สำนักงานโครงการหลักสูตร ศศ.บ.(ภาษาอังกฤษ) ภาคพิเศษ","ห้องเรียนโครงการจัดตั้งภาควิชาภาษาอังกฤษ"],["ห้องเรียนโครงการจัดตั้งภาควิชาภาษาอังกฤษ"],["ห้องเรียนโครงการจัดตั้งภาควิชาภาษาอังกฤษ"],["ห้องประชุม"]]
+
+const carouselSC1 = ["../../../assets/Images/Twine/Gallery/sc1/firstFloor.jpg", "../../../assets/Images/Twine/Gallery/sc1/chaiyaphuekExhibitionRoom.jpg", "../../../assets/Images/Twine/Gallery/sc1/geneticsEntrance.jpg", "../../../assets/Images/Twine/Gallery/sc1/biochemicalInstrument.jpg", "../../../assets/Images/Twine/Gallery/sc1/taobinMachine.jpg"];
+const carouselSC2 = [];
+const carouselSC3 = [];
+const carouselSC4 = [];
+const carouselSC5 = [];
+const carouselSC9 = ["../../../assets/Images/Twine/Gallery/sc9/1.jpg","../../../assets/Images/Twine/Gallery/sc9/2.jpg","../../../assets/Images/Twine/Gallery/sc9/3.jpg","../../../assets/Images/Twine/Gallery/sc9/4.jpg","../../../assets/Images/Twine/Gallery/sc9/5.jpg",];
+const carouselSC11 = ["../../../assets/Images/Twine/Gallery/sc11/1.jpg", "../../../assets/Images/Twine/Gallery/sc11/2.jpg", "../../../assets/Images/Twine/Gallery/sc11/3.jpg","../../../assets/Images/Twine/Gallery/sc11/4.jpg","../../../assets/Images/Twine/Gallery/sc11/5.jpg",];
+const carouselSC14 = [];
+const carouselLLB = [];
+
+const HSThemeAppearance = {
+    init() {
+        const defaultTheme = 'default'
+        let theme = localStorage.getItem('hs_theme') || defaultTheme
+
+        if (document.querySelector('html').classList.contains('dark')) return
+        this.setAppearance(theme)
+    },
+    _resetStylesOnLoad() {
+        const $resetStyles = document.createElement('style')
+        $resetStyles.innerText = `*{transition: unset !important;}`
+        $resetStyles.setAttribute('data-hs-appearance-onload-styles', '')
+        document.head.appendChild($resetStyles)
+        return $resetStyles
+    },
+    setAppearance(theme, saveInStore = true, dispatchEvent = true) {
+        const $resetStylesEl = this._resetStylesOnLoad()
+
+        if (saveInStore) {
+            localStorage.setItem('hs_theme', theme)
+        }
+
+        if (theme === 'auto') {
+            theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'default'
+        }
+
+        document.querySelector('html').classList.remove('dark')
+        document.querySelector('html').classList.remove('default')
+        document.querySelector('html').classList.remove('auto')
+
+        document.querySelector('html').classList.add(this.getOriginalAppearance())
+
+        setTimeout(() => {
+            $resetStylesEl.remove()
+        })
+
+        if (dispatchEvent) {
+            window.dispatchEvent(new CustomEvent('on-hs-appearance-change', {detail: theme}))
+        }
+    },
+    getAppearance() {
+        let theme = this.getOriginalAppearance()
+        if (theme === 'auto') {
+            theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'default'
+        }
+        return theme
+    },
+    getOriginalAppearance() {
+        const defaultTheme = 'default'
+        return localStorage.getItem('hs_theme') || defaultTheme
+    }
+}
+HSThemeAppearance.init()
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+    if (HSThemeAppearance.getOriginalAppearance() === 'auto') {
+        HSThemeAppearance.setAppearance('auto', false)
+    }
+})
+
+window.addEventListener('load', () => {
+    const $clickableThemes = document.querySelectorAll('[data-hs-theme-click-value]')
+    const $switchableThemes = document.querySelectorAll('[data-hs-theme-switch]')
+
+    $clickableThemes.forEach($item => {
+        $item.addEventListener('click', () => HSThemeAppearance.setAppearance($item.getAttribute('data-hs-theme-click-value'), true, $item))
+    })
+
+    $switchableThemes.forEach($item => {
+        $item.addEventListener('change', (e) => {
+            HSThemeAppearance.setAppearance(e.target.checked ? 'dark' : 'default')
+        })
+
+        $item.checked = HSThemeAppearance.getAppearance() === 'dark'
+    })
+
+    window.addEventListener('on-hs-appearance-change', e => {
+        $switchableThemes.forEach($item => {
+            $item.checked = e.detail === 'dark'
+        })
+    })
+})
+
+// Checks whether an element is currently visible on the screen
+function isElementVisible(element) {
+    const { top, bottom, left, right } = element.getBoundingClientRect();
+    const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+    const windowWidth = window.innerWidth || document.documentElement.clientWidth;
+    if (top <= windowHeight && bottom >= 0 && left <= windowWidth && right >= 0) {
+      return true;
+    }
+    return false;
+  }
+
+//This code section triggers moveUp animation when the text is supposed to be on screen
+document.addEventListener('DOMContentLoaded', () => {
+    const animatedParagraphs = document.querySelectorAll('.moveUp');
+    if (!animatedParagraphs) {
+      console.error("Could not find any elements to observe.");
+      return;
+    }
+    let delay = 0;
+    const delayIncrement = 175;
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          const animatedParagraph = entry.target;
+          animatedParagraph.classList.add('animate');
+          observer.unobserve(animatedParagraph);
+        }
+      });
+    }, { threshold: 0.2 });
+    for (const animatedParagraph of animatedParagraphs) {
+      const isVisible = isElementVisible(animatedParagraph);
+      if (isVisible) {
+        setTimeout(() => {
+          animatedParagraph.classList.add('animate');
+        }, delay);
+        delay += delayIncrement;
+      } else {
+        observer.observe(animatedParagraph);
+      }
+    }
+  });
+
+//This code section triggers moveToLeft animation when the text is supposed to be on screen
+document.addEventListener('DOMContentLoaded', () => {
+    const animatedParagraphs = document.querySelectorAll('.moveToLeft');
+    if (!animatedParagraphs) {
+      console.error("Could not find any elements to observe.");
+      return;
+    }
+    let delay = 0;
+    const delayIncrement = 175;
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          const animatedParagraph = entry.target;
+          animatedParagraph.classList.add('animate');
+          observer.unobserve(animatedParagraph);
+        }
+      });
+    }, { threshold: 0.2 });
+    for (const animatedParagraph of animatedParagraphs) {
+      const isVisible = isElementVisible(animatedParagraph);
+      if (isVisible) {
+        setTimeout(() => {
+          animatedParagraph.classList.add('animate');
+        }, delay);
+        delay += delayIncrement;
+      } else {
+        observer.observe(animatedParagraph);
+      }
+    }
+  });
